46594cbcb067e0fc1201d9d5364ab0ece6014a0e
Michael Okoe <michael.okoejr@gmail.com>
2022-08-18 21:21:10 +0000

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
+
 # Your code here!
+def greet_programmer ()
+  puts "Hello, programmer!"
+end
+
+
+def greet (name)
+  puts "Hello, #{name}!"
+end
+
+
+ def greet_with_default (name = "programmer")
+   puts "Hello, #{name}!"
+ end 
+
+
+def add (a, b)
+  puts a+b
+  return 2 + 5
+end 
+
+
+def halve (c)
+  if c.class !=Integer
+     return nil
+  end
+    c/2
+end
